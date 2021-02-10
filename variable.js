06e9b7c323c0067e27e584fdd5ab9edb39d0ff08
MD ZAWHARUL ISLAM <57909842+MOHAMMAD-ZAHIR@users.noreply.github.com>
2021-02-10 20:02:09 +0600

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,16 @@
+/**
+undefined, null,  number, boolean, bigint, object, string, symbol.
+ */
+
+ // declear a variable
+
+
+ var a = 1;
+
+ let b = 1;
+
+ const pi = 3.14;
+
+ // Use the var keyword to create a variable
+
+ // Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.
